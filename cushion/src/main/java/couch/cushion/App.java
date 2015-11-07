47b13eb6f7aba0189fa1e0d35ad6bad05966149b
CaptOkie <CaptOkie@users.noreply.github.com>
2015-11-07 18:04:05 -0500

    Setup project

---
+++ cushion/src/main/java/couch/cushion/App.java
@@ -0,0 +1,13 @@
+package couch.cushion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
