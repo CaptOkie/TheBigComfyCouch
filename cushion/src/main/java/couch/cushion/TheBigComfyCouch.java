e746e59ea5b7a4889298b0d77242a974959bfaf5
CaptOkie <CaptOkie@users.noreply.github.com>
2015-11-08 17:55:31 -0500

    Basic empty window

--- cushion/src/main/java/couch/cushion/TheBigComfyCouch.java
+++ cushion/src/main/java/couch/cushion/TheBigComfyCouch.java
@@ -1,18 +1,13 @@
 package couch.cushion;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  * Hello world!
  *
  */
-public class App extends Application
+public class TheBigComfyCouch extends Application
 {
     public static void main( String[] args )
     {
@@ -22,19 +17,7 @@ public class App extends Application
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("What up homies!");
-        Button button = new Button();
-        button.setText("Test me!");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("I did the thing!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-        primaryStage.setScene(new Scene(root, 300, 250));
+
         primaryStage.show();
     }
 }
