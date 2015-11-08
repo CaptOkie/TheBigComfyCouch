0c404a0293fee75dfa54c75c950737b1222620c2
CaptOkie <CaptOkie@users.noreply.github.com>
2015-11-07 20:07:59 -0500

    Start test

--- cushion/src/main/java/couch/cushion/App.java
+++ cushion/src/main/java/couch/cushion/App.java
@@ -1,13 +1,40 @@
 package couch.cushion;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("What up homies!");
+        Button button = new Button();
+        button.setText("Test me!");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("I did the thing!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
