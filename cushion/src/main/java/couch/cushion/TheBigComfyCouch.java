0eedead19915744216335c805966e340d3d6663a
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-11-11 21:43:08 -0500

    added a bit of a file browser

--- cushion/src/main/java/couch/cushion/TheBigComfyCouch.java
+++ cushion/src/main/java/couch/cushion/TheBigComfyCouch.java
@@ -1,22 +1,41 @@
 package couch.cushion;
 
-import java.io.IOException;
-
+import couch.cushion.ui.ImportMediaScene;
 import javafx.application.Application;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
  * Hello world!
  */
 public class TheBigComfyCouch extends Application {
-
-    public static void main(final String[] args) throws InterruptedException, IOException {
-         launch(args);
+    
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(final Stage primaryStage) throws Exception {
-        primaryStage.setTitle("What up homies!");
+    public void start(Stage primaryStage) throws Exception {
+        KeyCodeCombination ctrlQ = new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN);
+
+        /**
+         * Setting up main window
+         * Adding handler for ctrl+Q to for quit shorcut
+         */
+        primaryStage.setTitle("");
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
+            if (ctrlQ.match(e)) {
+                primaryStage.close();
+            }
+        });
+
+        /**
+         * Creation of the first scene/adding it to the window
+         */
+        primaryStage.setScene(new ImportMediaScene());
         primaryStage.show();
     }
 }
---
+++ cushion/src/main/java/couch/cushion/ui/ImportMediaScene.java
@@ -0,0 +1,69 @@
+package couch.cushion.ui;
+
+import java.io.File;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+public class ImportMediaScene extends Scene {
+
+    public ImportMediaScene() {
+        this(new GridPane());
+    }
+    
+    private ImportMediaScene(GridPane grid) {
+        super(grid);
+        
+        Text pathMessage = new Text();
+
+        TextField pathField = new TextField();
+        pathField.setPromptText("File Path");
+//        pathField.setOnKeyTyped(e -> {
+//            if (!Files.exists(Paths.get(pathField.getText()))) {
+//                
+//            }
+//        });
+        
+        Button search = new Button("Search");
+        search.setOnAction(e -> {
+            File file;
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Browse");
+            file = fileChooser.showOpenDialog(getWindow());
+            if (file != null) {
+                pathField.setText(file.getPath());
+            }
+        });
+
+
+        Button open = new Button("Open");
+        open.setOnAction(e -> {
+            pathMessage.setFill(Color.FIREBRICK);
+            pathMessage.setText("Invalid file.");
+        });
+        open.setDefaultButton(true);
+        
+        /**
+         * Setting up grid to align elements
+         */
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        /** 
+         * Adding elements to the grid
+         */
+        grid.add(pathField, 0, 0);
+        grid.add(search, 1, 0);
+        grid.add(pathMessage, 0, 2);
+        grid.add(open, 1, 1);
+    }
+}
