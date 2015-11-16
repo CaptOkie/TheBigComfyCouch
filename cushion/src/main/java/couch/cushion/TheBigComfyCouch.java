76baa7f0034b3323260e10ab8dea899e18a7dd48
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-11-15 21:37:53 -0500

    base scene set up

--- cushion/src/main/java/couch/cushion/TheBigComfyCouch.java
+++ cushion/src/main/java/couch/cushion/TheBigComfyCouch.java
@@ -2,10 +2,6 @@ package couch.cushion;
 
 import couch.cushion.ui.HomeScene;
 import javafx.application.Application;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -19,18 +15,10 @@ public class TheBigComfyCouch extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        KeyCodeCombination ctrlQ = new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN);
-
         /**
          * Setting up main window
-         * Adding handler for ctrl+Q to for quit shorcut
          */
         primaryStage.setTitle("");
-        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
-            if (ctrlQ.match(e)) {
-                primaryStage.close();
-            }
-        });
 
         /**
          * Creation of the first scene/adding it to the window
--- cushion/src/main/java/couch/cushion/ui/BaseScene.java
+++ cushion/src/main/java/couch/cushion/ui/BaseScene.java
@@ -3,27 +3,51 @@ package couch.cushion.ui;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
 
+/**
+ * BaseScene Base scene for scenes to inherit from. Has default settings.
+ * @author Juhandré Knoetze
+ *
+ */
 public abstract class BaseScene extends Scene {
-    
-    protected BaseScene() {
-        this(new VBox());
-    }
     
     protected final VBox    vbox;
     protected final MenuBar menuBar;
     protected final Menu    file;
+
+    /**
+     * Default Constructor for BaseScene
+     */
+    protected BaseScene() {
+        this(new VBox());
+    }
     
+    /**
+     * Initializes default settings for the scene.
+     * @param vbox Takes a vbox that will hold the menu bar along with othe scene nodes.
+     */
     private BaseScene(VBox vbox) {
         super(vbox, 300, 300);
+        KeyCodeCombination ctrlQ = new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN);
         
         this.vbox = vbox;
+
+        // Creating menu bar with file menu. Has 'Ctrl+Q' for quitting.
+        menuBar = new MenuBar();
+        file = new Menu("File");
+        MenuItem quit = new MenuItem("Quit");
+        quit.setAccelerator(ctrlQ);
+        quit.setOnAction(e -> {
+            System.exit(0);
+        });
+        file.getItems().add(quit);
+        menuBar.getMenus().add(file);
         
-        this.menuBar = new MenuBar();
-        this.file = new Menu("File");
-        this.menuBar.getMenus().add(this.file);
-        
-        vbox.getChildren().add(this.menuBar);
+        vbox.getChildren().add(menuBar);
     }
 }
