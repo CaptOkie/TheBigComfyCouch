cb22df1b98b19a1f3a324abfe6c7b5c8cb9eb0da
CaptOkie <CaptOkie@users.noreply.github.com>
2015-12-07 22:42:14 -0500

    Added custom video player to play images and audio

---
+++ cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
@@ -0,0 +1,37 @@
+package couch.cushion.ui;
+
+import java.nio.ByteBuffer;
+
+import io.humble.video.javaxsound.AudioFrame;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class VideoPlayer extends ImageView {
+
+    private AudioFrame audioFrame;
+    
+    public VideoPlayer() {
+        audioFrame = null;
+    }
+    
+    public void setAudioFrame(final AudioFrame audioFrame) {
+        if (audioFrame == null) {
+            throw new IllegalArgumentException("audioFrame is null");
+        }
+        this.audioFrame = audioFrame;
+    }
+    
+    public void play(final Image image) {
+        if (image == null) {
+            throw new IllegalArgumentException("image is null");
+        }
+        this.setImage(image); // TODO Maybe ensure the image is scaled to the size of the player if needed
+    }
+    
+    public void play(final ByteBuffer audio) {
+        if (audio == null) {
+            throw new IllegalArgumentException("audio is null");
+        }
+        audioFrame.play(audio);
+    }
+}
