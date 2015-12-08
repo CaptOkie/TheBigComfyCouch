bf9785a731077a59ed6e9f4346ecfa11b3a354aa
CaptOkie <CaptOkie@users.noreply.github.com>
2015-12-08 18:19:15 -0500

    updated video player

--- cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
+++ cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
@@ -2,6 +2,11 @@ package couch.cushion.ui;
 
 import java.nio.ByteBuffer;
 
+import javax.sound.sampled.AudioFormat;
+
+import io.humble.video.AudioChannel;
+import io.humble.video.AudioChannel.Layout;
+import io.humble.video.AudioFormat.Type;
 import io.humble.video.javaxsound.AudioFrame;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,7 +16,7 @@ public class VideoPlayer extends ImageView {
     private final AudioFrame audioFrame;
     
     public VideoPlayer() {
-        audioFrame = AudioFrame.make();
+        audioFrame = AudioFrame.make(new AudioFormat(22050, io.humble.video.AudioFormat.getBytesPerSample(Type.SAMPLE_FMT_S16)*8, AudioChannel.getNumChannelsInLayout(Layout.CH_LAYOUT_STEREO), true, false));
     }
     
     public void play(final Image image) {
