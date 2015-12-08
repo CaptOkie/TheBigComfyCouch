921c2e12162e5d716b151a70be37587406bc61c8
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-12-08 15:56:13 -0500

    renamed

--- cushion/src/main/java/couch/cushion/actor/message/FrameRate.java
+++ cushion/src/main/java/couch/cushion/actor/message/FrameRate.java
@@ -2,18 +2,18 @@ package couch.cushion.actor.message;
 
 import java.io.Serializable;
 
-public class SetFrameRate implements Serializable {
+public class FrameRate implements Serializable {
     
     private static final long serialVersionUID = -3268950497555304637L;
     
     private int numerator;
     private int denominator;
     
-    protected SetFrameRate() {
+    protected FrameRate() {
         this(0, 0);
     }
     
-    public SetFrameRate(final int numerator, final int denominator) {
+    public FrameRate(final int numerator, final int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
@@ -24,5 +24,9 @@ public class SetFrameRate implements Serializable {
     
     public int getNumerator() {
         return numerator;
+    }
+    
+    public double secondsPerFrame() {
+        return ((double) getDenominator()) / ((double) getNumerator());
     }
 }
