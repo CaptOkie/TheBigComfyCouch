2ee1ec69d30814a29ab74bd938adf0ac0f58e049
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-12-08 00:04:05 -0500

    updated MediaQueue

--- cushion/src/main/java/couch/cushion/actor/MediaQueue.java
+++ cushion/src/main/java/couch/cushion/actor/MediaQueue.java
@@ -1,16 +1,45 @@
 package couch.cushion.actor;
 
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+import java.util.Queue;
+
 import akka.actor.AbstractActor;
 import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
 import couch.cushion.ui.VideoPlayer;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.WritableImage;
 
 public class MediaQueue extends AbstractActor {
+
+    private VideoPlayer player;
+
+    private Queue<BufferedImage> imageBuffer;
+    private Queue<ByteBuffer> audioBuffer;
+
+    private boolean playing = false;
 
     public static Props props(final VideoPlayer player) {
         return Props.create(MediaQueue.class, () -> new MediaQueue(player));
     }
-        
+
     private MediaQueue(final VideoPlayer player) {
-        
+        this.player = player;
+        receive(ReceiveBuilder.match(BufferedImage.class, img -> handleImage(img)).match(ByteBuffer.class, audio -> handleAudio(audio)).build());
+    }
+
+    private void handleImage(BufferedImage img) {
+        WritableImage wimg = SwingFXUtils.toFXImage(img, null);
+        Platform.runLater(() -> {
+            player.play(wimg);
+        });
+    }
+
+    private void handleAudio(ByteBuffer audio) {
+        Platform.runLater(() -> {
+            player.play(audio);
+        });
     }
 }
