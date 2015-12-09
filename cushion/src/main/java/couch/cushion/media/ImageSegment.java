ad206dabf86efccf105539b9ebd3aa31b251816e
CaptOkie <CaptOkie@users.noreply.github.com>
2015-12-08 23:03:03 -0500

    image segment

--- cushion/src/main/java/couch/cushion/media/ImageSegment.java
+++ cushion/src/main/java/couch/cushion/media/ImageSegment.java
@@ -3,22 +3,24 @@ package couch.cushion.media;
 import java.io.Serializable;
 
 
-public class ImageSegment implements Serializable {
+public class ImageSegment implements Serializable, Comparable<ImageSegment> {
 
     private static final long serialVersionUID = 6119248475855211044L;
 
     private int id;
     private int index;
     private byte[] data;
+    private boolean last;
     
     protected ImageSegment() {
-        this (-1, -1, null);
+        this (-1, -1, null, false);
     }
     
-    public ImageSegment(final int id, final int index, final byte[] data) {
+    public ImageSegment(final int id, final int index, final byte[] data, final boolean last) {
         this.id = id;
         this.index = index;
         this.data = data;
+        this.last = last;
     }
     
     public int getId() {
@@ -31,5 +33,28 @@ public class ImageSegment implements Serializable {
     
     public byte[] getData() {
         return data;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+    
+    @Override
+    public int compareTo(final ImageSegment other) {
+        
+        if (getId() < other.getId()) {
+            return -1;
+        }
+        if (getId() > other.getId()) {
+            return 1;
+        }
+        
+        if (getIndex() < other.getIndex()) {
+            return -1;
+        }
+        if (getIndex() > other.getIndex()) {
+            return -1;
+        }
+        return 0;
     }
 }
