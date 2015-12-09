659e306dfc540927b98eec892855b54481688052
CaptOkie <CaptOkie@users.noreply.github.com>
2015-12-08 20:23:28 -0500

    limited video size

--- cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
+++ cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
@@ -6,6 +6,9 @@ import javafx.scene.image.ImageView;
 public class VideoPlayer extends ImageView {
 
     public VideoPlayer() {
+        this.setPreserveRatio(true);
+        this.setFitWidth(600);
+        this.setFitHeight(480);
     }
     
     public void play(final Image image) {
