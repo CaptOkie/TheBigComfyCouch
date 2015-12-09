36e6b3a8a62667a1d15feb0e9dc5de8634109021
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-12-08 19:04:40 -0500

    tweaks

--- cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
+++ cushion/src/main/java/couch/cushion/ui/VideoPlayer.java
@@ -1,22 +1,11 @@
 package couch.cushion.ui;
 
-import java.nio.ByteBuffer;
-
-import javax.sound.sampled.AudioFormat;
-
-import io.humble.video.AudioChannel;
-import io.humble.video.AudioChannel.Layout;
-import io.humble.video.AudioFormat.Type;
-import io.humble.video.javaxsound.AudioFrame;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class VideoPlayer extends ImageView {
 
-    private final AudioFrame audioFrame;
-    
     public VideoPlayer() {
-        audioFrame = AudioFrame.make(new AudioFormat(22050, io.humble.video.AudioFormat.getBytesPerSample(Type.SAMPLE_FMT_S16)*8, AudioChannel.getNumChannelsInLayout(Layout.CH_LAYOUT_STEREO), true, false));
     }
     
     public void play(final Image image) {
@@ -24,12 +13,5 @@ public class VideoPlayer extends ImageView {
             throw new IllegalArgumentException("image is null");
         }
         this.setImage(image); // TODO Maybe ensure the image is scaled to the size of the player if needed
-    }
-    
-    public void play(final ByteBuffer audio) {
-        if (audio == null) {
-            throw new IllegalArgumentException("audio is null");
-        }
-        audioFrame.play(audio);
     }
 }
