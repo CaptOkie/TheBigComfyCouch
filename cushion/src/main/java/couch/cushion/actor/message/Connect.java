cd1186da2a44c54ea2aefa3723b0bef7dcc0ee66
CaptOkie <CaptOkie@users.noreply.github.com>
2015-12-10 00:53:14 -0500

    stuff

--- cushion/src/main/java/couch/cushion/actor/message/Connect.java
+++ cushion/src/main/java/couch/cushion/actor/message/Connect.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 
 public class Connect implements Serializable {
 
+    private static final long serialVersionUID = -2276155176014443303L;
+
     private final String ip;
     
     public Connect(final String ip) {
