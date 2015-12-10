7fbb083cf508465185ae65c679156ebd0262b258
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-12-09 20:50:49 -0500

    updates

--- cushion/src/main/java/couch/cushion/actor/Master.java
+++ cushion/src/main/java/couch/cushion/actor/Master.java
@@ -10,6 +10,7 @@ import couch.cushion.actor.message.Pause;
 import couch.cushion.actor.message.Play;
 import couch.cushion.media.AudioData;
 import couch.cushion.media.ImageData;
+import couch.cushion.ui.HomeScene;
 import couch.cushion.ui.VideoPlayer;
 
 public class Master extends AbstractActor {
@@ -17,40 +18,42 @@ public class Master extends AbstractActor {
     private ActorRef mediaQueue;
     private ActorRef mediaDecoder;
     private ActorRef mediaTransport;
+    private ActorRef chatActor;
     
-    public static Props props(final VideoPlayer player) {
-        return Props.create(Master.class, () -> new Master(player));
+    public static Props props(final VideoPlayer player, final HomeScene homeScene) {
+        return Props.create(Master.class, () -> new Master(player, homeScene));
     }
 
-    private Master(final VideoPlayer player) {
+    private Master(final VideoPlayer player, final HomeScene homeScene) {
         
         mediaQueue = getContext().actorOf(MediaQueue.props(player), ActorConstants.MEDIA_QUEUE_NAME);
         mediaDecoder = getContext().actorOf(MediaDecoder.props(), ActorConstants.MEDIA_DECODER_NAME);
         mediaTransport = getContext().actorOf(MediaTransport.props().withDispatcher("media-transport-dispatcher"), ActorConstants.MEDIA_TRANSPORT_NAME);
-        
+        chatActor = getContext().actorOf(ChatActor.props(homeScene), ActorConstants.CHAT_ACTOR);
+                
         receive(
                 ReceiveBuilder.match(Decode.class, msg -> mediaDecoder.tell(msg, self()))
                 .match(Play.class, msg -> {
                     mediaTransport.tell(msg, self());
-//                    mediaQueue.tell(msg, self());
-//                    mediaDecoder.tell(msg, self());
+                    mediaQueue.tell(msg, self());
+                    mediaDecoder.tell(msg, self());
                 })
                 .match(Pause.class, msg -> {
                     mediaTransport.tell(msg, self());
-//                    mediaQueue.tell(msg, self());
-//                    mediaDecoder.tell(msg, self());
+                    mediaQueue.tell(msg, self());
+                    mediaDecoder.tell(msg, self());
                 })
                 .match(ImageData.class, msg -> {
                     mediaTransport.tell(msg, self());
-//                    mediaQueue.tell(msg, self());
+                    mediaQueue.tell(msg, self());
                 })
                 .match(AudioData.class, msg -> {
                     mediaTransport.tell(msg, self());
-//                    mediaQueue.tell(msg, self());
+                    mediaQueue.tell(msg, self());
                 })
                 .match(FrameRate.class, msg -> {
                     mediaTransport.tell(msg, self());
-//                    mediaQueue.tell(msg, self());
+                    mediaQueue.tell(msg, self());
                 })
                 .build()
             );
